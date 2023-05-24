bf9df2c749461fd3e6884563030ea510e2d30ff3
Clément Brossaud <clement.brossaud@isen-ouest.yncrea.fr>
2023-05-24 14:41:00 +0200

    displayWord fonctionne mais ne met pas de saut de ligne entre chaque br

--- src/js/index/index.js
+++ src/js/index/index.js
@@ -8,11 +8,29 @@ function displayWord() {
     wordElement.style.transitionDelay = `${index * 100}ms`;
     document.getElementById('who_we_are').appendChild(wordElement);
 
+    // Ajouter un espace après chaque mot, sauf pour le dernier mot
+    if (index < words.length - 1) {
+      const spaceElement = document.createElement('span');
+      spaceElement.innerHTML = '&nbsp;';
+      document.getElementById('who_we_are').appendChild(spaceElement);
+    }
+
+    // Vérifier s'il y a un élément <br> dans le texte
+    const nextElement = wordElement.nextElementSibling;
+    if (nextElement && nextElement.tagName === 'br') {
+      console.log('ca passe ici')
+      const spaceElement = document.createElement('span');
+      spaceElement.innerHTML = '&nbsp;';
+      document.getElementById('who_we_are').appendChild(spaceElement);
+      console.log('la aussi')
+    }
+
     setTimeout(() => {
       wordElement.classList.add('show');
     }, 100);
   });
 }
 
- 
-export { displayWord };
+displayWord();
+
+
