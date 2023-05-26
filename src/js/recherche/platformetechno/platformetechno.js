605ddde51cbeeb405f99a36c71463cfd30fd1470
Félix MARQUET <fpmovie4@gmail.com>
2023-05-26 18:31:05 +0200

    Little fix to the ticket effet, rework of the platforme technologique page, finish of the entier js part for this page (hope that the wifi from the TGV will be enought to commit and push)

---
+++ src/js/recherche/platformetechno/platformetechno.js
@@ -0,0 +1,43 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+    const platfTech = document.querySelectorAll('.Modal_Selector');
+    const modals = document.querySelectorAll('.modal');
+    const closeBtns = document.querySelectorAll('.close');
+    function displayModal(event) {
+        const platfTechIndex = Array.from(platfTech).findIndex((platforme) => platforme === event.currentTarget);
+        modals[platfTechIndex].style.display = 'block';
+        //Verification de l'id, si il correspond a LivingLab affiche les 150 premiers élément du text LivingLabText
+        if(modals[platfTechIndex].id === 'LivingLab'){
+            document.getElementById("LivingLabTextModal").innerHTML = truncateText("LivingLabText", 150);
+        }
+        if(modals[platfTechIndex].id === "EnvironnementHybridesConnectés"){
+            document.getElementById("EnvironnementHybridesConnectésTextModal").innerHTML = truncateText("EnvironnementHybridesConnectésText", 150);
+        }
+    }
+
+    function closeModal(event) {
+        const modal = event.currentTarget.closest('.modal');
+        modal.style.display = 'none';
+    }
+
+    platfTech.forEach(function (card) {
+        card.addEventListener('click', displayModal);
+    });
+
+    closeBtns.forEach(function (btn) {
+        btn.addEventListener('click', closeModal);
+    });
+
+    function truncateText(elementId, maxLength) {
+        var element = document.getElementById(elementId);
+        if (element) {
+            var text = element.textContent.trim();
+            if (text.length > maxLength) {
+                return text.substring(0, maxLength) + "...";
+            } else {
+                return text;
+            }
+        }
+        return "";
+    }
+});
