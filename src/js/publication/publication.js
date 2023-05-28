27a7797e4efa1241e34467c0c8d3e01c6f669aed
Clément Brossaud <clement.brossaud@isen-ouest.yncrea.fr>
2023-05-28 04:49:39 +0200

    Finish of the publication page

--- src/js/publication/publication.js
+++ src/js/publication/publication.js
@@ -14,3 +14,54 @@ function search() {
     }
   }
 }
+
+function revue(){
+  const searchTerm = 'pink square';   // recherche de 'pink square'
+  const paragraphs = document.querySelectorAll('.publi p'); // on prend tous les paragraphes
+  for (let i = 0; i < paragraphs.length; i++) { // boucle qui parcourt tous les paragraphes
+    const paragraph = paragraphs[i];
+    const images = paragraph.getElementsByTagName('img');  
+    let isPinkSquare = false; // on initialise la variable à false  
+    for (let j = 0; j < images.length; j++) {   // boucle qui parcourt toutes les images
+      const image = images[j];
+      const altText = image.getAttribute('alt').toLowerCase();  // on récupère le texte alternatif de l'image en minuscule
+      if (altText.includes(searchTerm)) {     // on cherche si le texte alternatif de l'image contient le terme recherché
+        isPinkSquare = true;
+        break;
+      }
+    }  
+    paragraph.style.display = isPinkSquare ? 'block' : 'none';    // si oui, on affiche le paragraphe, sinon on le cache
+  }
+}
+
+
+// meme fonction que revue
+function communication(){
+  const searchTerm = 'blue square';
+  const paragraphs = document.querySelectorAll('.publi p');
+  for (let i = 0; i < paragraphs.length; i++) {
+    const paragraph = paragraphs[i];
+    const images = paragraph.getElementsByTagName('img');  
+    let isBlueSquare = false;
+    for (let j = 0; j < images.length; j++) {
+      const image = images[j];
+      const altText = image.getAttribute('alt').toLowerCase();
+      if (altText.includes(searchTerm)) {
+        isBlueSquare = true;
+        break;
+      }
+    }  
+    paragraph.style.display = isBlueSquare ? 'block' : 'none';
+  }
+}
+
+
+//function reset
+
+function reset(){
+  const paragraphs = document.querySelectorAll('.publi p');
+  for (let i = 0; i < paragraphs.length; i++) {
+    const paragraph = paragraphs[i];
+    paragraph.style.display = 'block';
+  }
+}
