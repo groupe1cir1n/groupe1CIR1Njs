389ccbaf8c735a2925b97dc87ae5887f69bc9a81
Félix MARQUET <fpmovie4@gmail.com>
2023-05-28 09:34:10 +0100

    Add of the press enter to search

--- src/js/publication/publication.js
+++ src/js/publication/publication.js
@@ -1,19 +1,41 @@
-function search() {
-  const searchInput = document.getElementById('entry_text');
-  const searchTerm = searchInput.value.trim().toLowerCase();  // on récupère le terme recherché en minuscule
-  const paragraphs = document.querySelectorAll('.publi p');   // on prend tous les paragraphes
+function search(event) {
+  //ajout de la fonction entrée pour la recherche
+  if(event.key === 'Enter'){
+    const searchInput = document.getElementById('entry_text');
+    const searchTerm = searchInput.value.trim().toLowerCase();  // on récupère le terme recherché en minuscule
+    const paragraphs = document.querySelectorAll('.publi p');   // on prend tous les paragraphes
 
-  for (let i = 0; i < paragraphs.length; i++) {  // boucle qui parcourt tous les paragraphes
-    const paragraph = paragraphs[i];
-    const paragraphText = paragraph.textContent.toLowerCase(); // on récupère le texte du paragraphe en minuscule
+    for (let i = 0; i < paragraphs.length; i++) {  // boucle qui parcourt tous les paragraphes
+      const paragraph = paragraphs[i];
+      const paragraphText = paragraph.textContent.toLowerCase(); // on récupère le texte du paragraphe en minuscule
 
-    if (paragraphText.includes(searchTerm)) {   //on cherche si le texte du paragraphe contient le terme recherché
-      paragraph.style.display = 'block';      // si oui, on affiche le paragraphe
-    } else {
-      paragraph.style.display = 'none';    // sinon, on le cache
+      if (paragraphText.includes(searchTerm)) {   //on cherche si le texte du paragraphe contient le terme recherché
+        paragraph.style.display = 'block';      // si oui, on affiche le paragraphe
+      } else {
+        paragraph.style.display = 'none';    // sinon, on le cache
+      }
+    }
+  }
+  //ajout de la fonction click pour la recherche
+  else{
+    const searchInput = document.getElementById('entry_text');
+    const searchTerm = searchInput.value.trim().toLowerCase();  // on récupère le terme recherché en minuscule
+    const paragraphs = document.querySelectorAll('.publi p');   // on prend tous les paragraphes
+
+    for (let i = 0; i < paragraphs.length; i++) {  // boucle qui parcourt tous les paragraphes
+      const paragraph = paragraphs[i];
+      const paragraphText = paragraph.textContent.toLowerCase(); // on récupère le texte du paragraphe en minuscule
+
+      if (paragraphText.includes(searchTerm)) {   //on cherche si le texte du paragraphe contient le terme recherché
+        paragraph.style.display = 'block';      // si oui, on affiche le paragraphe
+      } else {
+        paragraph.style.display = 'none';    // sinon, on le cache
+      }
     }
   }
 }
+
+
 
 function revue(){
   const searchTerm = 'pink square';   // recherche de 'pink square'
